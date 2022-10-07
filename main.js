b83075a1b5438cae26bf20d1b5b2a8c73dd673cb
myo <0233023e@gmail.com>
2022-10-07 10:50:09 -0500

    Agregando el menu resposive para dispositivos celulares

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-let w = window
-let d = document
-let principalPosition = w.scrollY
+((d) => {
+    const $btnMenuOpen = d.querySelector('.menu-open'),
+        $btnMenuClose = d.querySelector('.menu-close')
 
-window.onscroll = function() {
-    let actualPosition = w.scrollY
-    if (principalPosition >= actualPosition) {
-        document.querySelector(".main-header").style.top = "0"
-    } else {
-        document.querySelector(".main-header").style.top = "-100px"
+    $btnMenuOpen.addEventListener("click", (e) => {
+        $btnMenuOpen.classList.toggle("none")
+        $btnMenuClose.classList.toggle("none")
+    })
+
+    $btnMenuClose.addEventListener("click", (e) => {
+        $btnMenuOpen.classList.toggle("none")
+        $btnMenuClose.classList.toggle("none")
+    })
+
+    let w = window
+    let principalPosition = w.scrollY
+
+    w.onscroll = function() {
+        let actualPosition = w.scrollY
+        if (principalPosition >= actualPosition) {
+            document.querySelector(".main-header").style.top = "0"
+        } else {
+            document.querySelector(".main-header").style.top = "-100px"
+        }
+        principalPosition = actualPosition
     }
-    principalPosition = actualPosition
-}
+})(document);
